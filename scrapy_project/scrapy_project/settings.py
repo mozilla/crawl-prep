e617577e9f4e1c3ff71e680be39d2a9d1cb8910a
Fredrik Wollsén <fredrik@neam.se>
2019-07-29 12:56:38 +0300

    Configure pre-crawl to mimic Fx68 better

--- scrapy_project/scrapy_project/settings.py
+++ scrapy_project/scrapy_project/settings.py
@@ -15,11 +15,11 @@ SPIDER_MODULES = ['scrapy_project.spiders']
 NEWSPIDER_MODULE = 'scrapy_project.spiders'
 
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'pre-crawl (+https://github.com/mozilla/crawl-prep)'
+# Set User Agent to mimic Firefox 68
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0'
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# Do not obey robots.txt rules
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 30
